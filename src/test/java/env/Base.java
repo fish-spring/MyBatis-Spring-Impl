d69d88d44d36b3146505bfa6990c5327a957a3ce
apple <wow@github.com>
2019-04-13 17:05:49 +0800

    init env  - spring jdbc,tx  - a bean of connections pool  - a sqlSesstionFactory bean

---
+++ src/test/java/env/Base.java
@@ -0,0 +1,19 @@
+package env;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class Base {
+    @Autowired
+    private SqlSessionFactory factory;
+
+    @Test
+    public void isEnvOk(){
+    }
+}
